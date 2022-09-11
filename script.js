6a99f7963ce971bf5dbb197f1aa94ac67f1ff7e5
Devon <devonross50@yahoo.com>
2022-09-11 11:40:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function clearTable(){
+    console.log("clear clicked")
+    var x = document.getElementsByTagName('td');
+    for (var i=0;i<x.length;i++) {
+        x[i].innerHTML = ''; 
+    }
+}
+
+function submitTable(){
+    console.log("Submit button clicked")
+}
